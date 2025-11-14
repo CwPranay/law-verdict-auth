aad69cef23af06e86ba59e8484657cf87a5a92a5
Pranay Gurav <guravpranay81@gmail.com>
2025-11-15 00:19:07 +0530

    login button updated

--- app/loginButton.tsx
+++ app/loginButton.tsx
@@ -7,9 +7,7 @@ export default function LoginButton() {
     <Button
       size="lg"
       onClick={() => (window.location.href = "/api/auth/login")}
-      className="bg-yellow-400 text-black font-semibold px-6 py-3 sm:px-8 sm:py-4 md:px-10 md:py-4 rounded-lg 
-                 hover:bg-yellow-500 active:bg-yellow-600 duration-200 text-sm sm:text-base md:text-lg 
-                 shadow-md hover:shadow-lg cursor-pointer"
+      className="bg-[#FFD700] py-6 px-8 text-black text-sm font-bold hover:bg-[#E6C200] transition  cursor-pointer"
     >
       Login
     </Button>
